8975390e14c6b264a0ecd581ecc0d7ed0a89b8c5
wolkan777 <41125385+wolkan77@users.noreply.github.com>
2019-01-17 19:52:08 +0300

    versiyon-1
    Yük kaybı hesabı eklendi ..
    Yapılan hesap hesap.txt adında bir dosyayla programın çalıştığı klasöre kaydedilebiliyor..

--- boru_cap_hesaplama/Form1.Designer.cs
+++ boru_cap_hesaplama/Form1.Designer.cs
@@ -36,12 +36,29 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 15);
+            this.label1.Location = new System.Drawing.Point(6, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
@@ -50,7 +67,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(91, 141);
+            this.label2.Location = new System.Drawing.Point(67, 152);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(130, 13);
             this.label2.TabIndex = 0;
@@ -58,14 +75,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 12);
+            this.textBox1.Location = new System.Drawing.Point(70, 23);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(121, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 115);
+            this.button1.Location = new System.Drawing.Point(70, 126);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 2;
@@ -81,7 +98,7 @@
             "16 atu",
             "20 atu",
             "25 atu"});
-            this.comboBox1.Location = new System.Drawing.Point(94, 81);
+            this.comboBox1.Location = new System.Drawing.Point(70, 92);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
@@ -89,7 +106,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 81);
+            this.label3.Location = new System.Drawing.Point(10, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 0;
@@ -98,7 +115,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 46);
+            this.label4.Location = new System.Drawing.Point(6, 57);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 0;
@@ -109,29 +126,176 @@
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
             "HDPE"});
-            this.comboBox2.Location = new System.Drawing.Point(94, 46);
+            this.comboBox2.Location = new System.Drawing.Point(70, 57);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 383);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÇAP SEÇİMİ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.comboBox3);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(265, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 382);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "YÜK KAYBI HESABI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Çıkış BTK";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Giriş BTK";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Mesafe";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(65, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(74, 232);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Hesap Çap (mm), Hız(m/s)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Boru Çapı";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(64, 159);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(156, 20);
+            this.textBox5.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(64, 125);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(156, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox3_MouseClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(65, 26);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Q(Debi l/s)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Yazdır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(375, 311);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(649, 424);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -145,6 +309,21 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- boru_cap_hesaplama/Form1.cs
+++ boru_cap_hesaplama/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Linq;
+using System.IO;
 
 namespace boru_cap_hesaplama
 {
@@ -12,19 +13,80 @@ namespace boru_cap_hesaplama
             InitializeComponent();
         }
 
+        #region Global Değişkenler
+        private Dictionary<int, double> cap_listesi_pn10 = new Dictionary<int, double>() { { 40, 35.2 }, { 50, 44 }, { 63, 55.4 }, { 75, 66 }, { 90, 79.2 }, { 110, 96.8 }, { 125, 110.2 }, { 140, 123.4 }, { 160, 141 }, { 180, 158.6 }, { 200, 176.2 }, { 225, 198.2 }, { 250, 220.4 }, { 280, 246.8 }, { 315, 277.6 }, { 355, 312.8 }, { 400, 352.6 }, { 450, 396.6 }, { 500, 440.6 }, { 560, 493.6 }, { 630, 555.2 }, { 710, 625.8 }, { 800, 705.2 }, { 900, 793.4 }, { 1000, 881.4 }, { 1200, 1058.8 }, { 1400, 1235.2 }, { 1600, 1411.8 } };
+        private Dictionary<int, double> cap_listesi_pn16 = new Dictionary<int, double>() { { 40, 32.6 }, { 50, 40.8 }, { 63, 51.4 }, { 75, 61.4 }, { 90, 73.6 }, { 110, 90 }, { 125, 102.2 }, { 140, 114.6 }, { 160, 130.8 }, { 180, 147.2 }, { 200, 163.6 }, { 225, 184 }, { 250, 204.6 }, { 280, 229.2 }, { 315, 257.8 }, { 355, 290.6 }, { 400, 327.4 }, { 450, 368.2 }, { 500, 409.2 }, { 560, 458.4 }, { 630, 515.6 }, { 710, 581 }, { 800, 654.6 }, { 900, 736.4 }, { 1000, 818.2 }, { 1200, 981.8 }, { 1400, 1145.4 }, { 1600, 1309 } };
+        private Dictionary<int, double> cap_listesi_pn20 = new Dictionary<int, double>() { { 40, 31 }, { 50, 38.8 }, { 63, 48.8 }, { 75, 58.2 }, { 90, 69.8 }, { 110, 85.4 }, { 125, 97 }, { 140, 108.6 }, { 160, 124.2 }, { 180, 139.8 }, { 200, 155.2 }, { 225, 174.6 }, { 250, 194.2 }, { 280, 217.4 }, { 315, 244.6 }, { 355, 275.6 }, { 400, 310.6 }, { 450, 349.4 }, { 500, 388.4 }, { 560, 435.6 }, { 630, 490 }, { 710, 552.4 }, { 800, 622.2 }, { 900, 700 }, { 1000, 777.8 }, { 1200, 933.4 }, { 1400, 1089 }, { 1600, 1244.4 } };
+        private Dictionary<int, double> cap_listesi_pn25 = new Dictionary<int, double>() { { 40, 29 }, { 50, 36.2 }, { 63, 45.8 }, { 75, 54.4 }, { 90, 65.4 }, { 110, 79.8 }, { 125, 90.8 }, { 140, 101.6 }, { 160, 116.2 }, { 180, 130.8 }, { 200, 145.2 }, { 225, 163.4 }, { 250, 181.6 }, { 280, 203.4 }, { 315, 228.8 }, { 355, 258 }, { 400, 290.6 }, { 450, 327 }, { 500, 364.8 }, { 560, 408.6 }, { 630, 459.8 }, { 710, 518.2 }, { 800, 583.8 }, { 900, 656.4 }, { 1000, 729.8 }, { 1200, 875.6 }, { 1400, 1021.6 }, { 1600, 1167.6 } };
+        private double debi;
+        private double hiz;
+        private double c_btk;
+        private double g_btk;
+        private double kot_farki;
+        private double mesafe;
+        private double cap;
+        private int C;
+
+        #endregion
+
+        private double HidrolikEgim()
+        {
+
+            debi = Convert.ToInt32(textBox4.Text);
+            C = 149;
+
+            if (kot_farki <= 100)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn10[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 160)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn16[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 200)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn20[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 250)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn25[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+
+            double h_egim = Math.Pow(((debi * 0.001) / (0.279 * C * Math.Pow((cap * 0.001), 2.63))), (1 / 0.54));
+            return h_egim;
+        }
+
+        private double Hiz()
+        {
+            debi = Convert.ToInt32(textBox4.Text);
+
+            if (kot_farki <= 100)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn10[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 160)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn16[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 200)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn20[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+            else if (kot_farki <= 250)
+            {
+                cap = Convert.ToDouble(cap_listesi_pn25[Convert.ToInt32(comboBox3.SelectedItem)]);
+            }
+
+            return (debi / 1000) / (Math.PI * (Math.Pow((cap / 1000), 2)) / 4);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             label2.Text = string.Empty;
-
-            Dictionary<int, double> cap_listesi_pn10 = new Dictionary<int, double>() { { 40, 35.2 }, { 50, 44 }, { 63, 55.4 }, { 75, 66 }, { 90, 79.2 }, { 110, 96.8 }, { 125, 110.2 }, { 140, 123.4 }, { 160, 141 }, { 180, 158.6 }, { 200, 176.2 }, { 225, 198.2 }, { 250, 220.4 }, { 280, 246.8 }, { 315, 277.6 }, { 355, 312.8 }, { 400, 352.6 }, { 450, 396.6 }, { 500, 440.6 }, { 560, 493.6 }, { 630, 555.2 }, { 710, 625.8 }, { 800, 705.2 }, { 900, 793.4 }, { 1000, 881.4 }, { 1200, 1058.8 }, { 1400, 1235.2 }, { 1600, 1411.8 } };
-            Dictionary<int, double> cap_listesi_pn16 = new Dictionary<int, double>() { { 40, 32.6 }, { 50, 40.8 }, { 63, 51.4 }, { 75, 61.4 }, { 90, 73.6 }, { 110, 90 }, { 125, 102.2 }, { 140, 114.6 }, { 160, 130.8 }, { 180, 147.2 }, { 200, 163.6 }, { 225, 184 }, { 250, 204.6 }, { 280, 229.2 }, { 315, 257.8 }, { 355, 290.6 }, { 400, 327.4 }, { 450, 368.2 }, { 500, 409.2 }, { 560, 458.4 }, { 630, 515.6 }, { 710, 581 }, { 800, 654.6 }, { 900, 736.4 }, { 1000, 818.2 }, { 1200, 981.8 }, { 1400, 1145.4 }, { 1600, 1309 } };
-            Dictionary<int, double> cap_listesi_pn20 = new Dictionary<int, double>() { { 40, 31 }, { 50, 38.8 }, { 63, 48.8 }, { 75, 58.2 }, { 90, 69.8 }, { 110, 85.4 }, { 125, 97 }, { 140, 108.6 }, { 160, 124.2 }, { 180, 139.8 }, { 200, 155.2 }, { 225, 174.6 }, { 250, 194.2 }, { 280, 217.4 }, { 315, 244.6 }, { 355, 275.6 }, { 400, 310.6 }, { 450, 349.4 }, { 500, 388.4 }, { 560, 435.6 }, { 630, 490 }, { 710, 552.4 }, { 800, 622.2 }, { 900, 700 }, { 1000, 777.8 }, { 1200, 933.4 }, { 1400, 1089 }, { 1600, 1244.4 } };
-            Dictionary<int, double> cap_listesi_pn25 = new Dictionary<int, double>() { { 40, 29 }, { 50, 36.2 }, { 63, 45.8 }, { 75, 54.4 }, { 90, 65.4 }, { 110, 79.8 }, { 125, 90.8 }, { 140, 101.6 }, { 160, 116.2 }, { 180, 130.8 }, { 200, 145.2 }, { 225, 163.4 }, { 250, 181.6 }, { 280, 203.4 }, { 315, 228.8 }, { 355, 258 }, { 400, 290.6 }, { 450, 327 }, { 500, 364.8 }, { 560, 408.6 }, { 630, 459.8 }, { 710, 518.2 }, { 800, 583.8 }, { 900, 656.4 }, { 1000, 729.8 }, { 1200, 875.6 }, { 1400, 1021.6 }, { 1600, 1167.6 } };
-
-            double debi = Convert.ToInt32(textBox1.Text);
-            double hiz;
+            debi = Convert.ToInt32(textBox1.Text);
 
             #region try
+
             try
             {
                 if (comboBox1.SelectedIndex == 0)
@@ -41,7 +103,7 @@ namespace boru_cap_hesaplama
                                 break;
                             }
                         }
-                        
+
                     }
                 }
                 else if (comboBox1.SelectedIndex == 1)
@@ -49,7 +111,7 @@ namespace boru_cap_hesaplama
                     for (int i = 0; i < cap_listesi_pn16.Count; i++)
                     {
                         hiz = (debi / 1000) / (Math.PI * (Math.Pow((cap_listesi_pn16.ElementAt(i).Value / 1000), 2)) / 4);
-                        if (hiz<3)
+                        if (hiz < 3)
                         {
                             label2.Text += cap_listesi_pn16.ElementAt(i).Key.ToString() + " mm" + " " + hiz.ToString("n3") + " m/s" + "\n";
                             if (hiz < 1.2)
@@ -58,7 +120,7 @@ namespace boru_cap_hesaplama
                                 break;
                             }
                         }
-                       
+
                     }
                 }
                 else if (comboBox1.SelectedIndex == 2)
@@ -84,7 +146,7 @@ namespace boru_cap_hesaplama
                     for (int i = 0; i < cap_listesi_pn25.Count; i++)
                     {
                         hiz = (debi / 1000) / (Math.PI * (Math.Pow((cap_listesi_pn25.ElementAt(i).Value / 1000), 2)) / 4);
-                        if (hiz<3)
+                        if (hiz < 3)
                         {
                             label2.Text += cap_listesi_pn25.ElementAt(i).Key.ToString() + " mm" + " " + hiz.ToString("n3") + " m/s" + "\n";
                             if (hiz < 1.2)
@@ -93,9 +155,10 @@ namespace boru_cap_hesaplama
                                 break;
                             }
                         }
-                       
+
                     }
                 }
+
             }
             #endregion
 
@@ -104,6 +167,88 @@ namespace boru_cap_hesaplama
 
                 MessageBox.Show(x.Message);
             }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label8.Text = string.Empty;
+            c_btk = Convert.ToInt32(textBox2.Text);
+            g_btk = Convert.ToInt32(textBox3.Text);
+            kot_farki = c_btk - g_btk;
+            mesafe = Convert.ToInt32(textBox5.Text);
+            double hidrolik_egim = HidrolikEgim();
+            hiz = Hiz();
+            if (kot_farki < 0)
+            {
+                MessageBox.Show("Terfi Hattı Hesabı Daha Sonra Güncellenecektir!!");
+            }
+            else
+            {
+
+                label8.Text = "Giriş BTK ------> " + g_btk.ToString("n2") + " m" + "\n" +
+                    "Çıkış BTK ------> " + c_btk.ToString("n2") + " m" + "\n" +
+                    "Hidrolik Eğim, J ------> " + hidrolik_egim.ToString("n6") + " m/m" + "\n" +
+                    "Hız ------>  " + hiz.ToString("n3") + " m/s" + "\n" +
+                    "Giriş PK ------> " + g_btk.ToString("n2") + " m" + "\n" +
+                    "Çıkış PK ------> " + (c_btk - g_btk - (hidrolik_egim * mesafe)).ToString("n2") + " m";
+
+            }
+
+        }
+
+        private void comboBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            comboBox3.Items.Clear();
+            try
+            {
+                if (kot_farki <= 100)
+                {
+                    for (int i = 0; i < cap_listesi_pn10.Count; i++)
+                    {
+                        comboBox3.Items.Add(cap_listesi_pn10.ElementAt(i).Key);
+                    }
+                }
+                else if (kot_farki <= 160)
+                {
+                    for (int i = 0; i < cap_listesi_pn16.Count; i++)
+                    {
+                        comboBox3.Items.Add(cap_listesi_pn16.ElementAt(i).Key);
+                    }
+                }
+                else if (kot_farki <= 200)
+                {
+                    for (int i = 0; i < cap_listesi_pn20.Count; i++)
+                    {
+                        comboBox3.Items.Add(cap_listesi_pn20.ElementAt(i).Key);
+                    }
+                }
+                else if (kot_farki <= 250)
+                {
+                    for (int i = 0; i < cap_listesi_pn25.Count; i++)
+                    {
+                        comboBox3.Items.Add(cap_listesi_pn25.ElementAt(i).Key);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("25 atu üzeri basınç için standart üretim HDPE boru bulunmamaktadır!");
+                }
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show(x.Message);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] yazilacak_metin = label8.Text.Split('\n');
+            File.WriteAllLines(Application.StartupPath + @"\hesap.txt", yazilacak_metin);
+            MessageBox.Show("Hesap Dosyası Yazıldı!");
 
         }
     }
